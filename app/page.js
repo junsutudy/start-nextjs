3510a74b0d3eaf89c5271911d23b221f5dca29ea
junsuPark <krpjs0508@gmail.com>
2024-09-23 10:25:19 +0900

    link

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   console.log("Executing..");
   return (
@@ -5,6 +7,9 @@ export default function Home() {
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
+      <p>
+        <Link href="/about">About us</Link>
+      </p>
     </main>
   );
 }
