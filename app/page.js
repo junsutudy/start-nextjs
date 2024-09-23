91e32cd8277e0ed84fc24a74bc5ea9e7216dd53e
junsuPark <krpjs0508@gmail.com>
2024-09-23 11:19:48 +0900

    header

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 import Link from "next/link";
 
+import Header from "./header";
+
 export default function Home() {
   console.log("Executing..");
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
+      <Header />
       <p>🔥 Let&apos;s get started! 🔥</p>
       <p>
         <Link href="/about">About us</Link>
